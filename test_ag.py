8cb4607d8f2712bf9e2edc19541c03013e0e958f
AG <malejandrogarcia@gmail.com>
2019-09-18 18:19:52 -0300

    Más tests sobre el método distance()

--- test_ag.py
+++ test_ag.py
@@ -44,3 +44,39 @@ def test_distance_B_02():
     dist_ac = gsp.distance(np.array(p_a), np.array([p_c]))
     dist_bc = gsp.distance(np.array(p_b), np.array([p_c]))
     assert dist_ac <= dist_ab + dist_bc
+
+def test_distance_C_01():
+    # Distancias >= 0 (metric='euclid')
+    data = np.random.uniform(-10, 10, size=(50, 2))
+    periodic = {0: None, 1: None}
+    gsp = GriSPy(data=data, N_cells=2, copy_data=False, periodic=periodic, metric='euclid', load_grid=None)
+    centre_0 = np.random.uniform(-10, 10, size=(2,))
+    dist = gsp.distance(centre_0, data)
+    assert (dist >= 0).all()
+
+def test_distance_C_02():
+    # Distancias >= 0 (metric='sphere')
+    data = np.random.uniform(-10, 10, size=(50, 2))
+    periodic = {0: None, 1: None}
+    gsp = GriSPy(data=data, N_cells=2, copy_data=False, periodic=periodic, metric='sphere', load_grid=None)
+    centre_0 = np.random.uniform(-10, 10, size=(2,))
+    dist = gsp.distance(centre_0, data)
+    assert (dist >= 0).all()
+
+def test_distance_D_01():
+    # Distancias != NaN (metric='euclid')
+    data = np.random.uniform(-10, 10, size=(50, 2))
+    periodic = {0: None, 1: None}
+    gsp = GriSPy(data=data, N_cells=2, copy_data=False, periodic=periodic, metric='euclid', load_grid=None)
+    centre_0 = np.random.uniform(-10, 10, size=(2,))
+    dist = gsp.distance(centre_0, data)
+    assert not np.isnan(dist).any()
+
+def test_distance_D_02():
+    # Distancias != NaN (metric='sphere')
+    data = np.random.uniform(-10, 10, size=(50, 2))
+    periodic = {0: None, 1: None}
+    gsp = GriSPy(data=data, N_cells=2, copy_data=False, periodic=periodic, metric='sphere', load_grid=None)
+    centre_0 = np.random.uniform(-10, 10, size=(2,))
+    dist = gsp.distance(centre_0, data)
+    assert not np.isnan(dist).any()
