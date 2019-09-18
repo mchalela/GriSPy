93788e843b0317830bb1bfac8a9a04913da2dd27
AG <malejandrogarcia@google.com>
2019-09-18 13:50:13 -0300

    Archivo test_ag con cuatro tests sobre el método distance()

---
+++ test_ag.py
@@ -0,0 +1,46 @@
+import numpy as np
+from grispy import GriSPy
+
+def test_distance_A_01():
+    # Distancia a-->b = b-->a (metric='euclid')
+    data = np.random.uniform(-10, 10, size=(10, 2))
+    periodic = {0: None, 1: None}
+    gsp = GriSPy(data=data, N_cells=2, copy_data=False, periodic=periodic, metric='euclid', load_grid=None)
+    dist1 = gsp.distance(np.array([1,1]), np.array([[2,2]]))
+    dist2 = gsp.distance(np.array([2,2]), np.array([[1,1]]))
+    np.testing.assert_almost_equal(dist1, dist2, decimal=16)
+
+def test_distance_A_02():
+    # Distancia a-->b = b-->a (metric='sphere')
+    data = np.random.uniform(-10, 10, size=(10, 2))
+    periodic = {0: None, 1: None}
+    gsp = GriSPy(data=data, N_cells=2, copy_data=False, periodic=periodic, metric='sphere', load_grid=None)
+    dist1 = gsp.distance(np.array([1,1]), np.array([[2,2]]))
+    dist2 = gsp.distance(np.array([2,2]), np.array([[1,1]]))
+    np.testing.assert_almost_equal(dist1, dist2, decimal=16)
+
+def test_distance_B_01():
+    # Distancia a-->c <= a-->b + b-->c (metric='euclid')
+    data = np.random.uniform(-10, 10, size=(10, 2))
+    periodic = {0: None, 1: None}
+    gsp = GriSPy(data=data, N_cells=2, copy_data=False, periodic=periodic, metric='euclid', load_grid=None)
+    p_a = [1,1]
+    p_b = [2,2]
+    p_c = [1,2]
+    dist_ab = gsp.distance(np.array(p_a), np.array([p_b]))
+    dist_ac = gsp.distance(np.array(p_a), np.array([p_c]))
+    dist_bc = gsp.distance(np.array(p_b), np.array([p_c]))
+    assert dist_ac <= dist_ab + dist_bc
+
+def test_distance_B_02():
+    # Distancia a-->c <= a-->b + b-->c (metric='sphere')
+    data = np.random.uniform(-10, 10, size=(10, 2))
+    periodic = {0: None, 1: None}
+    gsp = GriSPy(data=data, N_cells=2, copy_data=False, periodic=periodic, metric='sphere', load_grid=None)
+    p_a = [1,1]
+    p_b = [2,2]
+    p_c = [1,2]
+    dist_ab = gsp.distance(np.array(p_a), np.array([p_b]))
+    dist_ac = gsp.distance(np.array(p_a), np.array([p_c]))
+    dist_bc = gsp.distance(np.array(p_b), np.array([p_c]))
+    assert dist_ac <= dist_ab + dist_bc
