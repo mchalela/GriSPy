28eb3be2ae7d30ed41659f06e2936d6e87512940
Emanuel Sillero <g.emanuel.sillero@gmail.com>
2019-12-05 08:58:06 -0300

    add requirements.txt to docs for Read the Docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,7 +64,7 @@ master_doc = 'index'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', '**.ipynb_checkpoints']
+exclude_patterns = ['**.ipynb_checkpoints']
 
 # -- Options for HTML output -------------------------------------------------
 
