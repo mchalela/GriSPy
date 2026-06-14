5a4c6089ae236ae2f02e2e87c287df464be44cfe
mchalela <tinchochalela@gmail.com>
2026-06-14 17:17:22 -0300

    updating grispy to pyproject.toml only. dropping setup.py

--- grispy/__init__.py
+++ grispy/__init__.py
@@ -12,7 +12,7 @@
 GriSPy is a regular grid search algorithm for quick nearest-neighbor lookup.
 """
 
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 
 
 # =============================================================================
